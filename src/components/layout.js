0d3ff132b80daf8356cb54a1eedec9da78f3ad09
Alvin Crespo <alvin.crespo@gmail.com>
2020-05-17 21:48:21 -0400

    configures purge selection

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,14 +9,14 @@ class Layout extends React.Component {
     const rootPath = `${__PATH_PREFIX__}/`
     const isRootPath = location.pathname === rootPath
 
-    const headerClassNames = [
-      `${isRootPath ? "text-2xl sm:text-4xl" : "text-xl sm:text-2xl"}`,
-    ]
-
     return (
       <div className="p-6 border-3">
         <header className="mb-6">
-          <h1 className={headerClassNames}>
+          <h1
+            className={
+              isRootPath ? "text-2xl sm:text-4xl" : "text-xl sm:text-2xl"
+            }
+          >
             <Link to={`/`}>{title}</Link>
           </h1>
         </header>
