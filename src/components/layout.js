1e87e9f9c3cbe7c4d50276b56e060a156bae89af
Alvin Crespo <alvin.crespo@gmail.com>
2020-07-26 10:19:11 -0400

    sync global styles w/ main site

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
 import Bio from "../components/bio"
-import "./layout.css"
+import "../styles/global.css"
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    const isRootPath = location.pathname === rootPath
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
 
-    return (
-      <div className="p-6 border-3">
-        <header className="mb-6">
-          <h1
-            className={
-              isRootPath ? "text-2xl sm:text-4xl" : "text-xl sm:text-2xl"
-            }
-          >
-            <Link to={`/`}>{title}</Link>
-          </h1>
-        </header>
-        <main className="max-w-3xl">{children}</main>
-        <footer>
-          <Bio />
-        </footer>
-      </div>
-    )
-  }
+  return (
+    <div className="p-6 border-3">
+      <header className="mb-6">
+        <h1
+          className={
+            isRootPath ? "text-2xl sm:text-4xl" : "text-xl sm:text-2xl"
+          }
+        >
+          <Link to={`/`}>{title}</Link>
+        </h1>
+      </header>
+      <main className="max-w-3xl">{children}</main>
+      <footer>
+        <Bio />
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
