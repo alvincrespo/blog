674d529206a1dc21ac369869ec304fe5dac388cd
Alvin Crespo <alvin.crespo@gmail.com>
2020-07-26 10:15:51 -0400

    match tailwind config w/ main site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   purge: ["./src/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        deeppink: "deeppink",
+        cyan: "cyan",
+        yellow: "yellow",
+      },
+    },
+    screens: {
+      xsm: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   variants: {},
   plugins: [],
